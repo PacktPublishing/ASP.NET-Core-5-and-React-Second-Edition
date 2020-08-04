dd2b4c65325e4af63b4a6a04f5cbd7486013a0b3
carl <carl.rippon@theaccessgroup.com>
2020-08-04 20:23:14 +0100

    chapter 6

---
+++ chapter-06/frontend/src/QuestionsData.ts
@@ -0,0 +1,139 @@
+export interface QuestionData {
+  questionId: number;
+  title: string;
+  content: string;
+  userName: string;
+  created: Date;
+  answers: AnswerData[];
+}
+export interface AnswerData {
+  answerId: number;
+  content: string;
+  userName: string;
+  created: Date;
+}
+
+const questions: QuestionData[] = [
+  {
+    questionId: 1,
+    title: 'Why should I learn TypeScript?',
+    content:
+      'TypeScript seems to be getting popular so I wondered whether it is worth my time learning it? What benefits does it give over JavaScript?',
+    userName: 'Bob',
+    created: new Date(),
+    answers: [
+      {
+        answerId: 1,
+        content:
+          'To catch problems earlier speeding up your developments',
+        userName: 'Jane',
+        created: new Date(),
+      },
+      {
+        answerId: 2,
+        content:
+          'So, that you can use the JavaScript features of tomorrow, today',
+        userName: 'Fred',
+        created: new Date(),
+      },
+    ],
+  },
+  {
+    questionId: 2,
+    title:
+      'Which state management tool should I use?',
+    content:
+      'There seem to be a fair few state management tools around for React - React, Unstated, ... Which one should I use?',
+    userName: 'Bob',
+    created: new Date(),
+    answers: [],
+  },
+];
+
+export const getUnansweredQuestions = async (): Promise<
+  QuestionData[]
+> => {
+  await wait(500);
+  return questions.filter(
+    (q) => q.answers.length === 0,
+  );
+};
+
+const wait = async (
+  ms: number,
+): Promise<void> => {
+  return new Promise((resolve) =>
+    setTimeout(resolve, ms),
+  );
+};
+
+export const getQuestion = async (
+  questionId: number,
+): Promise<QuestionData | null> => {
+  await wait(500);
+  const results = questions.filter(
+    (q) => q.questionId === questionId,
+  );
+  return results.length === 0 ? null : results[0];
+};
+
+export const searchQuestions = async (
+  criteria: string,
+): Promise<QuestionData[]> => {
+  await wait(500);
+  return questions.filter(
+    (q) =>
+      q.title
+        .toLowerCase()
+        .indexOf(criteria.toLowerCase()) >= 0 ||
+      q.content
+        .toLowerCase()
+        .indexOf(criteria.toLowerCase()) >= 0,
+  );
+};
+
+export interface PostQuestionData {
+  title: string;
+  content: string;
+  userName: string;
+  created: Date;
+}
+
+export const postQuestion = async (
+  question: PostQuestionData,
+): Promise<QuestionData | undefined> => {
+  await wait(500);
+  const questionId =
+    Math.max(
+      ...questions.map((q) => q.questionId),
+    ) + 1;
+  const newQuestion: QuestionData = {
+    ...question,
+    questionId,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  return newQuestion;
+};
+
+export interface PostAnswerData {
+  questionId: number;
+  content: string;
+  userName: string;
+  created: Date;
+}
+
+export const postAnswer = async (
+  answer: PostAnswerData,
+): Promise<AnswerData | undefined> => {
+  await wait(500);
+  const question = questions.filter(
+    (q) => q.questionId === answer.questionId,
+  )[0];
+  const answerInQuestion: AnswerData = {
+    answerId: 99,
+    ...answer,
+  };
+  question.answers.push(answerInQuestion);
+  return answerInQuestion;
+};
