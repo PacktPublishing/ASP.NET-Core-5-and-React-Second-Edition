87a9da225d2815eab8496eb8a88db7d388c9ea15
carl <carl.rippon@theaccessgroup.com>
2020-11-11 11:07:38 +0000

    updated questions

--- chapter-08/start/backend/SQLScripts/01-Tables.sql
+++ chapter-08/start/backend/SQLScripts/01-Tables.sql
@@ -40,14 +40,14 @@ VALUES(1, 'Why should I learn TypeScript?',
     'TypeScript seems to be getting popular so I wondered whether it is worth my time learning it? What benefits does it give over JavaScript?',
     '1',
     'bob.test@test.com',
-    '2019-05-18 14:32')
+    '2021-01-18 14:32')
 
 INSERT INTO dbo.Question(QuestionId, Title, Content, UserId, UserName, Created)
 VALUES(2, 'Which state management tool should I use?', 
     'There seem to be a fair few state management tools around for React - React, Unstated, ... Which one should I use?',
     '2',
     'jane.test@test.com',
-    '2019-05-18 14:48')
+    '2021-01-18 14:48')
 GO
 SET IDENTITY_INSERT dbo.Question OFF
 GO
@@ -55,10 +55,10 @@ GO
 SET IDENTITY_INSERT dbo.Answer ON 
 GO
 INSERT INTO dbo.Answer(AnswerId, QuestionId, Content, UserId, UserName, Created)
-VALUES(1, 1, 'To catch problems earlier speeding up your developments', '2', 'jane.test@test.com', '2019-05-18 14:40')
+VALUES(1, 1, 'To catch problems earlier speeding up your developments', '2', 'jane.test@test.com', '2021-01-18 14:40')
 
 INSERT INTO dbo.Answer(AnswerId, QuestionId, Content, UserId, UserName, Created)
-VALUES(2, 1, 'So, that you can use the JavaScript features of tomorrow, today', '3', 'fred.test@test.com', '2019-05-18 16:18')
+VALUES(2, 1, 'So, that you can use the JavaScript features of tomorrow, today', '3', 'fred.test@test.com', '2021-01-18 16:18')
 GO
 SET IDENTITY_INSERT dbo.Answer OFF 
 GO
